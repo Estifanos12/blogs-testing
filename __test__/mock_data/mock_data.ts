176abfa579b883a5b4ce6963b062bad080b20b14
estifanos12 <estif17gtb@gmail.com>
2023-05-29 20:44:47 +0300

    Moving the mock data in separate file

---
+++ __test__/mock_data/mock_data.ts
@@ -0,0 +1,26 @@
+export default {
+    mockPostData: {
+        title: "Eyuel Ketema",
+        description: "some description about eyu ketema",
+        vote: 6,
+        author: ""
+    },
+
+    mockPostData2: {
+        title: "Eyuel Ketema",
+        description: "some description about eyu ketema",
+        vote: "6",
+        author: "Estifanos Gashawtena"
+    },
+    mockUserData: {
+        username: "alemu",
+        name: "Eyuel ketema",
+        email: "e@gmail.com"
+    },
+
+    mockUserData2: {
+        username: "alemusisay",
+        name: "Eyuel ketema",
+        email: "e@gmail.com"
+    }
+};
