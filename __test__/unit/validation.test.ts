b1a9e41c4723e6839bc83c47fa09e535942390fa
estifanos12 <estif17gtb@gmail.com>
2023-05-29 20:45:09 +0300

    Adding test for validation

---
+++ __test__/unit/validation.test.ts
@@ -0,0 +1,31 @@
+import { validatePostRequest } from "../../src/validation/post.validation";
+import { validateUserRequest } from "../../src/validation/user.validation";
+import mock_data from "../mock_data/mock_data";
+
+describe("Post request validation", () => {
+    describe("author field is empty", () => {
+        it("should return error message", async () => {
+            expect(await validatePostRequest(mock_data.mockPostData)).toEqual({ message: '"author" is not allowed to be empty' });
+        });
+
+        describe("vote field is empty", () => {
+            it("should return error message", async () => {
+                expect(await validatePostRequest(mock_data.mockPostData2)).toEqual({ message: '"vote" must be a number' });
+            });
+        });
+    });
+});
+
+describe("User request validation", () => {
+    describe("Username is less than 6 characters", () => {
+        it("should return error message", async () => {
+            expect(await validateUserRequest(mock_data.mockUserData)).toEqual({ message: '"username" length must be at least 6 characters long' });
+        });
+
+        describe("if the email is valid", () => {
+            it("should return success message", async () => {
+                expect(await validateUserRequest(mock_data.mockUserData2)).toEqual({ message: "Success" });
+            });
+        });
+    });
+});
