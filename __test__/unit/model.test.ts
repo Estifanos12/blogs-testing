b6765e5e230f30214a2acddd429492225ff1bb94
estifanos12 <estif17gtb@gmail.com>
2023-05-29 20:57:31 +0300

    Changing model test description

--- __test__/unit/model.test.ts
+++ __test__/unit/model.test.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { Post } from "../../src/models/post.model";
 import { User } from "../../src/models/user.model";
 import { MONGO_URL } from "../../src/config";
+import mock_data from "../mock_data/mock_data";
 
 beforeAll(async () => {
     await mongoose.connect(MONGO_URL);
@@ -12,46 +13,22 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-const correctUserMockData = {
-    username: "someusername1234",
-    name: "some name",
-    email: "someemail1234@gmail.com"
-};
-
-const requiredMissingUserMockData = {
-    username: "username123",
-    name: "some namenew"
-};
-
-const correctPostMockData = {
-    title: "aabcdegffa",
-    description: "anjfhadhjadjhf",
-    vote: 5,
-    author: "646ca147a83e24d475fbf660"
-};
-
-const inCorrectPostMockData = {
-    title: "aabcdegffa",
-    description: "anjfhadhjadjhf",
-    vote: 5,
-    author: "incorrectID"
-};
 describe("Testing models", () => {
     describe("Testing User Model", () => {
         it("should create & save user successfully", async () => {
-            const newUser = new User(correctUserMockData);
+            const newUser = new User(mock_data.correctUserMockData);
             const savedUser = await newUser.save();
             expect(savedUser._id).toBeDefined();
-            expect(savedUser.username).toBe(correctUserMockData.username);
-            expect(savedUser.name).toBe(correctUserMockData.name);
-            expect(savedUser.email).toBe(correctUserMockData.email);
+            expect(savedUser.username).toBe(mock_data.correctUserMockData.username);
+            expect(savedUser.name).toBe(mock_data.correctUserMockData.name);
+            expect(savedUser.email).toBe(mock_data.correctUserMockData.email);
         }, 60000);
 
         it("should throw an error due to missing requied fields", async () => {
-            const newUser = new User(requiredMissingUserMockData);
+            const newUser = new User(mock_data.requiredMissingUserMockData);
             let err;
             try {
-                const savedUserWithoutRequiredField = await newUser.save();
+                await newUser.save();
             } catch (error) {
                 err = error;
             }
@@ -61,19 +38,19 @@ describe("Testing models", () => {
 
     describe("Testing Post Model", () => {
         it("Should create and save post if user exists", async () => {
-            const newPost = new Post(correctPostMockData);
+            const newPost = new Post(mock_data.correctPostMockData);
             const savedPost = await newPost.save();
             expect(savedPost._id).toBeDefined();
-            expect(savedPost.title).toBe(correctPostMockData.title);
-            expect(savedPost.description).toBe(correctPostMockData.description);
-            expect(savedPost.vote).toBe(correctPostMockData.vote);
+            expect(savedPost.title).toBe(mock_data.correctPostMockData.title);
+            expect(savedPost.description).toBe(mock_data.correctPostMockData.description);
+            expect(savedPost.vote).toBe(mock_data.correctPostMockData.vote);
         }, 60000);
 
         it("should throw an error due to invalid user id", async () => {
-            const newPost = new User(inCorrectPostMockData);
+            const newPost = new User(mock_data.invalidPostMockData);
             let err;
             try {
-                const userDoesNotExist = await newPost.save();
+                await newPost.save();
             } catch (error) {
                 err = error;
             }
