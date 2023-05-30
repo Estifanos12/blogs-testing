20c7769da9fba3abfa1d24c36f84e619afbd2626
estifanos12 <estif17gtb@gmail.com>
2023-05-30 12:57:24 +0300

    Adding validation testcases

--- __test__/unit/validation.test.ts
+++ __test__/unit/validation.test.ts
@@ -1,16 +1,19 @@
+import { ValidationError } from "joi";
+
 import { validatePostRequest } from "../../src/validation/post.validation";
 import { validateUserRequest } from "../../src/validation/user.validation";
+import { validateAuthUserRequest } from "../../src/validation/auth.validation";
 import mock_data from "../mock_data/mock_data";
 
 describe("Post request validation", () => {
     describe("author field is empty", () => {
-        it("should return error message", async () => {
-            expect(await validatePostRequest(mock_data.mockPostData)).toEqual({ message: '"author" is not allowed to be empty' });
+        it("should return error message", () => {
+            expect(validatePostRequest(mock_data.mockPostData)).toBeInstanceOf(ValidationError);
         });
 
         describe("vote field is not a number", () => {
-            it("should return error message", async () => {
-                expect(await validatePostRequest(mock_data.mockPostData2)).toEqual({ message: '"vote" must be a number' });
+            it("should return error message", () => {
+                expect(validatePostRequest(mock_data.mockPostData2)).toBeInstanceOf(ValidationError);
             });
         });
     });
@@ -18,14 +21,22 @@ describe("Post request validation", () => {
 
 describe("User request validation", () => {
     describe("username is less than 6 characters", () => {
-        it("should return error message", async () => {
-            expect(await validateUserRequest(mock_data.mockUserData)).toEqual({ message: '"username" length must be at least 6 characters long' });
+        it("should return error message", () => {
+            expect(validateUserRequest(mock_data.mockUserData)).toBeInstanceOf(ValidationError);
         });
+    });
 
-        describe("if the email is valid", () => {
-            it("should return success message", async () => {
-                expect(await validateUserRequest(mock_data.mockUserData2)).toEqual({ message: "Success" });
-            });
+    describe("the email is valid", () => {
+        it("should return success message", () => {
+            expect(validateUserRequest(mock_data.mockUserData2)).toEqual({ message: "Success" });
+        });
+    });
+});
+
+describe("User auth validation", () => {
+    describe("email not valid format", () => {
+        it("should return error message", () => {
+            expect(validateAuthUserRequest(mock_data.invalidEmailFormat)).toBeInstanceOf(ValidationError);
         });
     });
 });
