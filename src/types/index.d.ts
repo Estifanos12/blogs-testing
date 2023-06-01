c76ed796ced98a41e599f55d1b1a2c351962091f
estifanos12 <estif17gtb@gmail.com>
2023-06-01 09:16:21 +0300

    Adding user payload

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,11 +1,11 @@
-import { UserPayload } from '../interfaces/user.interface'
+import { UserPayload } from "../interfaces/user.interface";
 // to make the file a module and avoid the TypeScript error
-export {}
+export {};
 
 declare global {
-  namespace Express {
-    export interface Request {
-      user: UserPayload;
+    namespace Express {
+        export interface Request {
+            user: UserPayload;
+        }
     }
-  }
 }
